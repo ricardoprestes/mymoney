ec66c6e79df4828a18742035184a3d52609081c4
Ricardo Prestes <ricardo.logan@hotmail.com>
2022-07-14 17:46:16 -0300

    Create balance card layout

--- src/Views/DashboardPage.xaml.cs
+++ src/Views/DashboardPage.xaml.cs
@@ -2,23 +2,9 @@
 
 public partial class DashboardPage : ContentPage
 {
-	int count = 0;
-
 	public DashboardPage()
 	{
 		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
 }
 
