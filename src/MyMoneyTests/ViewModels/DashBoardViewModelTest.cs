86d1b8be3ca60278e2396e1cf33aad221e8fb369
Ricardo Prestes <ricardo.logan@hotmail.com>
2022-07-20 15:22:48 -0300

    Adding first default test

---
+++ src/MyMoneyTests/ViewModels/DashBoardViewModelTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentAssertions;
+using MyMoney.ViewModels;
+
+namespace MyMoneyTests.ViewModels
+{
+    public class DashBoardViewModelTest
+    {
+        [Fact]
+        public void ShouldBeDefaultValues()
+        {
+            var viewModel = new DashboardViewModel();
+
+            viewModel.Balance.Should().Be(default);
+            viewModel.Revenues.Should().Be(default);
+            viewModel.Expenses.Should().Be(default);
+        }
+    }
+}
+
