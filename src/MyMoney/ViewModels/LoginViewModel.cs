2f2b242b70d8a5f0e250fa2b7be4d9a21ccc7abb
Ricardo Prestes <ricardo.logan@hotmail.com>
2022-07-21 15:20:05 -0300

    Create initial login flow

---
+++ src/MyMoney/ViewModels/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CommunityToolkit.Mvvm.Input;
+using MyMoney.Views;
+
+namespace MyMoney.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+
+        public LoginViewModel()
+        {
+            LoginCommand = new AsyncRelayCommand(Login);
+        }
+
+        public IAsyncRelayCommand LoginCommand { get; }
+
+        public async Task Login()
+        {
+            await Shell.Current.GoToAsync($"//{nameof(DashboardPage)}");
+        }
+    }
+}
+
