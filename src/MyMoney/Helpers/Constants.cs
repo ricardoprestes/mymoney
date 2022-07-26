333079faa9b236bfc4c79382b033f344ff25f3d2
Ricardo Prestes <ricardo.logan@hotmail.com>
2022-07-26 11:52:21 -0300

    Add FirebaseWebApiKey constant

---
+++ src/MyMoney/Helpers/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MyMoney.Helpers
+{
+    public static class Constants
+    {
+        public const string FirebaseWebApiKey = "AIzaSyB0n_F38sf1QkNBlOP12Qzh7_YCFlL-6-4";
+    }
+}
+
